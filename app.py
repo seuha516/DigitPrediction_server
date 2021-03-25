91bfdd7fbe20d266ecf081f9e2a8c2470dc3ae27
seuha516 <seuha516@naver.com>
2021-03-25 14:00:01 +0900

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ CORS(app)
 import numpy as np
 import cv2
 from PIL import Image
+from model import *
 
 def image_convert(img):
     img = cv2.cvtColor(np.array(img), cv2.COLOR_BGR2GRAY)
@@ -24,10 +25,10 @@ def number():
     img = image_convert(Image.open(request.files['file'].stream))
     if(np.sum(img)<1.0): return "?, ?"
     return "99, 99"
-    # result = pred(img)
-    # answer = np.argmax(result)
-    # prob = result[0][answer]*100.0
-    # return str(answer) + ", " + str(round(prob,2))
+    result = pred(img)
+    answer = np.argmax(result)
+    prob = result[0][answer] * 100.0
+    return str(answer) + ", " + str(round(prob, 2))
 
 if __name__ == "__main__":
     app.run()
