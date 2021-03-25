fa7d3e8ef547958223251a73b8b86339ffffd604
seuha516 <seuha516@naver.com>
2021-03-25 13:30:38 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
 from flask import Flask, request
 from flask_cors import CORS
-app = Flask (__name__)
+app = Flask(__name__, static_url_path='/static')
+UPLOAD_FOLDER ='static/uploads/'
+DOWNLOAD_FOLDER = 'static/downloads/'
+ALLOWED_EXTENSIONS = {'jpg', 'png', '.jpeg'}
+# APP CONFIGURATIONS
+app.config['SECRET_KEY'] = 'YourSecretKey'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['DOWNLOAD_FOLDER'] = DOWNLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 2 * 1024 * 1024
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 CORS(app)
 
 import numpy as np
