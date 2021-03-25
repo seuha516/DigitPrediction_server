cc2eaf8230d6d38409af8eb7d10fb304523b2216
seuha516 <seuha516@naver.com>
2021-03-25 13:56:26 +0900

    .

--- app.py
+++ app.py
@@ -5,10 +5,29 @@ CORS(app)
 
 import numpy as np
 import cv2
+from PIL import Image
 
-@app.route("/")
+def image_convert(img):
+    img = cv2.cvtColor(np.array(img), cv2.COLOR_BGR2GRAY)
+    img = cv2.resize(np.array(img), (28, 28), interpolation=cv2.INTER_AREA)
+    img = 255 - (img)
+    img = img / 255
+    img.reshape((28, 28, 1))
+    return img
+
+@app.route('/')
 def hello():
-    return "Hello World!"
+    return 'Hello!'
+
+@app.route('/number', methods=['POST'])
+def number():
+    img = image_convert(Image.open(request.files['file'].stream))
+    if(np.sum(img)<1.0): return "?, ?"
+    return "99, 99"
+    # result = pred(img)
+    # answer = np.argmax(result)
+    # prob = result[0][answer]*100.0
+    # return str(answer) + ", " + str(round(prob,2))
 
 if __name__ == "__main__":
     app.run()
